cffd39e2b31034f569d5683ea9222aba7450304f
Larysa <Larysa76@gmail.com>
2019-06-04 15:13:49 +0300

    Updates

---
+++ precache-manifest.cb2f746b94f0ec794004899390aa0d5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6cd8428b39d66d372294e68e457b673",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "f6f446ac26da04b5568d",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.407f4c6c.chunk.css"
+  },
+  {
+    "revision": "628fde99d9b46faf4765",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b7e46c99.chunk.js"
+  },
+  {
+    "revision": "f6f446ac26da04b5568d",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.520bde70.chunk.js"
+  },
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  }
+]);
